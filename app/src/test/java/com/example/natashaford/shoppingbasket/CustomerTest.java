d3c7ffb12e0c77a840bc3d965c15c27244b73508
Natasha Ford <neford3991@gmail.com>
2017-07-21 11:54:52 +0100

    Initial set up of project.

---
+++ app/src/test/java/com/example/natashaford/shoppingbasket/CustomerTest.java
@@ -0,0 +1,15 @@
+package com.example.natashaford.shoppingbasket;
+
+import org.junit.Before;
+
+/**
+ * Created by natashaford on 21/07/2017.
+ */
+
+public class CustomerTest {
+
+    @Before
+    void setup(){
+
+    }
+}
